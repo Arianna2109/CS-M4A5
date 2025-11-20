2afa7b3718c0149099cc38bfb52a6cde0012a959
Arianna2109 <ariannapeguero.21@gmail.com>
2025-11-20 21:43:03 +0000

    This is the assignment for CS11 M4A5. It solves the area of a circle using the radius, then it solves for the a and b value. Lastly, it creates an immag of a cute cat!

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+#1 part
+import math
+radius = (int(input("what is the radius?")))
 
+area = (str(math.pi * (radius**2)))
+print("your area is:"+str(area)) 
+
+#2nd part
+a=(int(input("What is the a value?")))
+b=(int(input("What is the b value")))
+
+answer = (str((a-b)+(a*b)))
+print("Your answer is: "+str(answer))
+
+print(" /\ _ /\ ")
+print("( 0 . 0 )")
+print("  > ^ <")
